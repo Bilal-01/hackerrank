917abb063a79990b0cd21aa95bbbd1e05dbed361
Bilal-01 <mbilalaziz.01@gmail.com>
2023-07-25 19:47:31 +0500

    utopian tree - easy

---
+++ problem-solving/easy/utopianTree.py
@@ -0,0 +1,15 @@
+def utopianTree(n):
+    if n == 0: 
+        return 1
+    
+    height = 1
+    for i in range(1, n+1):
+        if i%2 == 0:
+            height += 1
+        else:
+            height *= 2
+
+    return height
+
+n = 5
+print(utopianTree(n))
